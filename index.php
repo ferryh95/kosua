086d3d244d4b0fb80ab40e8c8fce2249e2e81e10
Ferry <ferryip895@gmail.com>
2023-09-19 15:27:52 +0900

    version 1.0(beta)

--- index.php
+++ index.php
@@ -13,11 +13,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+HK&family=Noto+Sans+JP&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
-
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/924a4290f9.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -116,11 +118,16 @@
                         <div class="pick-up-text">
                             Relax at hot springs and shrines in winter!
                         </div>
-                        <div class="pick-ip-text-2 text-right">
-                            <div class="read-this">read this article</div>
-                            <div class="pick-up-button-class">
-                                <button class="pick-up-button"></button>
+                        <div class="pickup-check text-right">
+                            <div class="pickup-check-text">
+                                read this article
                             </div>
+                            <div class="button-pickup-check">
+                                <button>
+                                    <img src="images/common/right-awrrow.png" alt="矢印">
+                                </button>
+                            </div>
+
                         </div>
                         <hr>
                         <div class="emergency-alert">
@@ -256,6 +263,225 @@
 
                 </div>
             </div>
+            <!--article-->
+            <div class="article text-center">
+                <div class="article-text">
+                    Article
+                </div>
+                <div class="article-text-bottom">
+                    New feature article
+                </div>
+                <div class="article-detail">
+                    <div class="row">
+                        <div class="col text-left">
+                            <div class="detail-image">
+                                <img src="images/top/dammy.jpg" alt="ダミー">
+                            </div>
+                            <div class="detail-text text-left">
+                                Get out of the city and enjoy luxury during your trip.
+                            </div>
+                            <div class="detail-date text-left">
+                                2023.10.10 up
+                            </div>
+                            <span class="detail-tag">transport</span>
+                            <span class="detail-tag">＃Oita</span>
+                        </div>
+                        <div class="col text-left">
+                            <div class="detail-image">
+                                <img src="images/top/dammy.jpg" alt="ダミー">
+                            </div>
+                            <div class="detail-text text-left">
+                                Get out of the city and enjoy luxury during your trip.
+                            </div>
+                            <div class="detail-date text-left">
+                                2023.10.10 up
+                            </div>
+                            <span class="detail-tag">transport</span>
+                            <span class="detail-tag">＃Oita</span>
+                        </div>
+                        <div class="col text-left">
+                            <div class="detail-image">
+                                <img src="images/top/dammy.jpg" alt="ダミー">
+                            </div>
+                            <div class="detail-text text-left">
+                                Get out of the city and enjoy luxury during your trip.
+                            </div>
+                            <div class="detail-date text-left">
+                                2023.10.10 up
+                            </div>
+                            <span class="detail-tag">transport</span>
+                            <span class="detail-tag">＃Oita</span>
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+                    <div class="row">
+                        <div class="col text-left">
+                            <div class="detail-image">
+                                <img src="images/top/dammy.jpg" alt="ダミー">
+                            </div>
+                            <div class="detail-text text-left">
+                                Get out of the city and enjoy luxury during your trip.
+                            </div>
+                            <div class="detail-date text-left">
+                                2023.10.10 up
+                            </div>
+                            <span class="detail-tag">transport</span>
+                            <span class="detail-tag">＃Oita</span>
+                        </div>
+                        <div class="col text-left">
+                            <div class="detail-image">
+                                <img src="images/top/dammy.jpg" alt="ダミー">
+                            </div>
+                            <div class="detail-text text-left">
+                                Get out of the city and enjoy luxury during your trip.
+                            </div>
+                            <div class="detail-date text-left">
+                                2023.10.10 up
+                            </div>
+                            <span class="detail-tag">transport</span>
+                            <span class="detail-tag">＃Oita</span>
+                        </div>
+                        <div class="col text-left">
+                            <div class="detail-image">
+                                <img src="images/top/dammy.jpg" alt="ダミー">
+                            </div>
+                            <div class="detail-text text-left">
+                                Get out of the city and enjoy luxury during your trip.
+                            </div>
+                            <div class="detail-date text-left">
+                                2023.10.10 up
+                            </div>
+                            <span class="detail-tag">transport</span>
+                            <span class="detail-tag">＃Oita</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="article-check text-right">
+                <div class="article-check-text">
+                    View more articles
+                </div>
+                <div class="button-article-check">
+                    <button>
+                        <img src="images/common/right-awrrow.png" alt="矢印">
+                    </button>
+                </div>
+            </div>
+            <!--Event-->
+            <div class="event text-center">
+                <div class="event-text">
+                    Events
+                </div>
+                <div class="event-text-bottom">
+                    Introducing recommended events
+                </div>
+                <div class="swiper swiper2">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide text-left">
+                            <div class="e-swipper-image">
+                                <img src="images/top/" alt="リビュー">
+                            </div>
+                            <div class="e-swipper-text">
+                                Grand Setsubun Festival
+                            </div>
+                            <div class="e-swipper-loc">
+                                <i class="fa-solid fa-location-dot"></i>Kushida Shrine
+                            </div>
+                        </div>
+                        <div class="swiper-slide text-left">
+                            <div class="e-swipper-image">
+                                <img src="images/top/review-img.jpg" alt="リビュー">
+                            </div>
+                            <div class="e-swipper-text">
+                                Grand Setsubun Festival
+                            </div>
+                            <div class="e-swipper-loc">
+                                <i class="fa-solid fa-location-dot"></i>Kushida Shrine
+                            </div>
+                        </div>
+                        <div class="swiper-slide text-left">
+                            <div class="e-swipper-image">
+                                <img src="images/top/review-img.jpg" alt="リビュー">
+                            </div>
+                            <div class="e-swipper-text">
+                                Grand Setsubun Festival
+                            </div>
+                            <div class="e-swipper-loc">
+                                <i class="fa-solid fa-location-dot"></i>Kushida Shrine
+                            </div>
+                        </div>
+                        <div class="swiper-slide text-left">
+                            <div class="e-swipper-image">
+                                <img src="images/top/review-img.jpg" alt="リビュー">
+                            </div>
+                            <div class="e-swipper-text">
+                                Grand Setsubun Festival
+                            </div>
+                            <div class="e-swipper-loc">
+                                <i class="fa-solid fa-location-dot"></i>Kushida Shrine
+                            </div>
+                        </div>
+                        <div class="swiper-slide text-left">
+                            <div class="e-swipper-image">
+                                <img src="images/top/review-img.jpg" alt="リビュー">
+                            </div>
+                            <div class="e-swipper-text">
+                                Grand Setsubun Festival
+                            </div>
+                            <div class="e-swipper-loc">
+                                <i class="fa-solid fa-location-dot"></i>Kushida Shrine
+                            </div>
+                        </div>
+                        <div class="swiper-slide text-left">
+                            <div class="e-swipper-image">
+                                <img src="images/top/review-img.jpg" alt="リビュー">
+                            </div>
+                            <div class="e-swipper-text">
+                                Grand Setsubun Festival
+                            </div>
+                            <div class="e-swipper-loc">
+                                <i class="fa-solid fa-location-dot"></i>Kushida Shrine
+                            </div>
+                        </div>
+                        <div class="swiper-slide text-left">
+                            <div class="e-swipper-image">
+                                <img src="images/top/review-img.jpg" alt="リビュー">
+                            </div>
+                            <div class="e-swipper-text">
+                                Grand Setsubun Festival
+                            </div>
+                            <div class="e-swipper-loc">
+                                <i class="fa-solid fa-location-dot"></i>Kushida Shrine
+                            </div>
+                        </div>
+                        <div class="swiper-slide text-left">
+                            <div class="e-swipper-image">
+                                <img src="images/top/review-img.jpg" alt="リビュー">
+                            </div>
+                            <div class="e-swipper-text">
+                                Grand Setsubun Festival
+                            </div>
+                            <div class="e-swipper-loc">
+                                <i class="fa-solid fa-location-dot"></i>Kushida Shrine
+                            </div>
+                        </div>
+                        <div class="swiper-slide text-left">
+                            <div class="e-swipper-image">
+                                <img src="images/top/review-img.jpg" alt="リビュー">
+                            </div>
+                            <div class="e-swipper-text">
+                                Grand Setsubun Festival
+                            </div>
+                            <div class="e-swipper-loc">
+                                <i class="fa-solid fa-location-dot"></i>Kushida Shrine
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+
         </main>
         <footer></footer>
     </div>
@@ -269,6 +495,7 @@
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
     <script src="js/main.js"></script>
+
 </body>
 
 </html>
--- js/main.js
+++ js/main.js
@@ -1,44 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
-    const success = (position) => {
-        console.log(position);
-        const lat = position.coords.latitude;
-        const long = position.coords.lonitude;
 
-        const geroAPIURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en'
-        
-        fetch(geroAPIURL)
-            .then(res => res.json())
-            .then(data => { console.log(data.city); getWeatherData(data.city)});
-    }
-        
-    const error = () => {
-        alert('Unable to retrieve your location');
-    }
+  const success = (position) => {
+    console.log(position);
+    const lat = position.coords.latitude;
+    const long = position.coords.lonitude;
+
+    const geroAPIURL = 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en'
+
+    fetch(geroAPIURL)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.city);
+        getWeatherData(data.city)
+      });
+  }
+
+  const error = () => {
+    alert('Unable to retrieve your location');
+  }
 
   navigator.geolocation.getCurrentPosition(success, error);
-  
-  
+
+
   /*date*/
   const date = new Date();
   let year = date.getFullYear();
-  let day = date.toLocaleDateString('en-EN', { weekday: 'short' })
+  let day = date.toLocaleDateString('en-EN', {
+    weekday: 'short'
+  })
   let month = date.getMonth() + 1;
-  let dates = date.getDate(); 
+  let dates = date.getDate();
   console.log(dates);
 
-  const yeartxt = document.querySelector("#year"); 
+  const yeartxt = document.querySelector("#year");
   const daytxt = document.querySelector("#day");
   const monthtext = document.querySelector("#mondate");
   const datestext = document.querySelector("#daydate");
   yeartxt.textContent = year;
-  daytxt.textContent= day;
+  daytxt.textContent = day;
   monthtext.textContent = month;
   datestext.textContent = dates;
 });
 
 function getWeatherData(location) {
-    console.log(location);
+  console.log(location);
   const apiKey = "9ed2191923d3aedfe5f28b2baa27c194";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
   return fetch(url)
@@ -52,6 +57,7 @@ function getWeatherData(location) {
       updateUI(weatherData);
     });
 }
+
 function updateUI(weatherData) {
   const temperature = document.querySelector("#temperature");
   const condition = document.querySelector("#condition");
@@ -64,11 +70,21 @@ function updateUI(weatherData) {
 
 
 /*swipper*/
- var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+/*event swipper*/
+var swiper2 = new Swiper(".swiper2", {
+  slidesPerView: 3,
+  spaceBetween: 60,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
